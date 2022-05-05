ae7b38c63e55505a22a09fe6506969923bc32a28
Kiiza Atwiine Stephen <stephenkiiza123@gmail.com>
2022-05-05 16:58:00 +0300

    Products Management API

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,26 +1,50 @@
 import { v4 as uuidv4 } from 'uuid';
 
 // CRUD
-let products = [{
-    "name": "Shoes",
-    "quantity": 1,
-}];
+let products = [
+
+    {
+        "name": "Shoes",
+        "color": "Red",
+        "quantity": 10,
+        "quality": "Standard",
+        "manufacturer": "Nike",
+        "price": 14000,
+        "remaining stock": 120,
+        "address": {
+            "location": "Kampala",
+            "Shop/Mall": "Shoprite"
+        },
+        "id": uuidv4(),
+    }
+
+];
 
 // Create Product.
 export const createProduct = (req, res) => {
     const product = req.body;
     products.push({...product, id: uuidv4() })
-    res.status(200).send(`Product with ID ${product.id} has been added to database.`)
+    res.status(200).send(`Product with ID ${req.params} has been added to database.`)
 }
 
-// // Get All Product.
-// export function getAllProducts(req, res) {}
+// Get All Product.
+export const getAllProducts = (req, res) => {
+    res.send(products);
+}
 
-// // Get specific Product.
-// export function getSpecificProduct(req, res) {}
+// Get specific Product.
+export const getSpecificProduct = (req, res) => {
+    const { id } = req.params;
+    const foundProduct = products.find((product) => product.id === id);
+    res.status(200).send(foundProduct);
+}
 
 // // Update Product.
 // export function updateProduct(req, res) {}
 
-// // Delete Product.
-// export function deleteProduct(req, res) {}
+// Delete Product.
+export const deleteProduct = (req, res) => {
+    const { id } = req.params;
+    products = products.filter((product) => product.id !== id);
+    res.status(200).send(`Product with ID ${id} has been deleted.`)
+}
--- index.js
+++ index.js
@@ -11,10 +11,10 @@ const PORT = process.env.PORT || 3000;
 // Middleware
 app.use(bodyParser.json())
 app.use(cors());
+app.use('/products', productRouter);
 
 // Routes
 app.get('/', (req, res) => { res.status(200).send('Welcome to the Products Management API v2') })
-app.get('/products', productRouter);
 
 // Server Initialization.
 app.listen(PORT, () => { console.log(`Server Application running at http://localhost:${PORT}`); });
